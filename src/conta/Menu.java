1e77757756e1bd6d973e4caf592119e8ca551439
Larissa Sanches <larissaesanches@gmail.com>
2024-02-21 10:17:52 -0300

    Atualização do projeto

--- src/conta/Menu.java
+++ src/conta/Menu.java
@@ -1,10 +1,25 @@
 package conta;
 
+import conta.model.Conta;
+
 public class Menu {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+		// Teste da Calsse Conta
+		Conta conta1 = new Conta(1456, 123, 1, "Larissa Sanches", 50000.0f);
+
+		conta1.visualizar();
+		conta1.setAgencia(456);
+		System.out.println(conta1.getAgencia());
+
+		if (conta1.sacar(1000))
+			System.out.println("\nSaque efetuado com sucesso. O novo saldo é de: " + conta1.getSaldo());
+		else
+			System.out.println("\nO Saldo é insuficiente!");
+
+		conta1.depositar(1000);
+		System.out.println("\nO novo saldo é: " + conta1.getSaldo());
 
 	}
-
 }
